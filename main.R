4230656ecef4b8a5ae7873edb3fb6c35006689f8
agouy <contact@agouy.com>
2021-01-08 14:20:57 +0000

    first commit

--- main.R
+++ main.R
@@ -1,9 +1,8 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+(ctx = tercenCtx()) %>% 
+  select(.x, .y) %>%
+  transmute(product = .y * .x) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,14 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "01cb95cd7b746443ed9f40625200ef4f")
+options("tercen.stepId"     = "20a59891-7ac9-4b48-8124-2b46ac24a549")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+(ctx = tercenCtx()) %>% 
+  select(.x, .y) %>%
+  transmute(product = .y * .x) %>%
   ctx$addNamespace() %>%
   ctx$save()
